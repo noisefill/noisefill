bf14cea1f3effc5ad290c946388ea5dd72866495
George <g3eorge@pm.me>
2025-08-23 00:16:51 -0400

    Part 2 of removing loops

--- src/auth.ts
+++ src/auth.ts
@@ -5,7 +5,6 @@ import { accounts, sessions, users, verificationTokens } from "@/db/schema";
 import GitHub from "next-auth/providers/github";
 import Figma from "next-auth/providers/figma";
 import Google from "next-auth/providers/google";
-import Loops from "next-auth/providers/loops";
 import Discord from "next-auth/providers/discord";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
@@ -16,10 +15,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     verificationTokensTable: verificationTokens,
   }),
   providers: [
-    Loops({
-      apiKey: process.env.AUTH_LOOPS_KEY!,
-      transactionalId: process.env.AUTH_LOOPS_TRANSACTIONAL_ID!,
-    }) as any,
     GitHub,
     Figma,
     Google,
